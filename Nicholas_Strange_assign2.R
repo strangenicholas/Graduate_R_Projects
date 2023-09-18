a0a9eb629f01b9847ad5b7d7e739d67c8eed3232
strangenicholas <nicholaswstrange@gmail.com>
2023-09-18 18:04:21 -0400

    Assign 2 init

---
+++ Nicholas_Strange_assign2.R
@@ -0,0 +1,6 @@
+# A) As noted above, the dataset mret7018 provides monthly stock price data over the 1970–
+# 2018 (see the file mret_var for definitions). Using the price variable (find it), construct
+# monthly stock returns for each stock each month based on the formula:
+# 
+# B) Does the stock market perform differently during the month of February in a leap year
+# vs. a common year? 
