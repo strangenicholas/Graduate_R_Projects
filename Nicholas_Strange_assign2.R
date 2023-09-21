48bb604caa07c6ee98e50ca9e305b5afa59e57a7
strangenicholas <nicholaswstrange@gmail.com>
2023-09-21 16:12:24 -0400

    added leap year and visuals

--- Nicholas_Strange_assign2.R
+++ Nicholas_Strange_assign2.R
@@ -18,15 +18,24 @@ mret <-
   mutate(year = as.numeric(format(DATE,"%Y")),
          month = as.numeric(format(DATE,"%m")),
          PRC = abs(PRC),
+         AdjPRC = PRC/CFACPR,
          mcap = PRC*SHROUT,
          mcap1 = lag(mcap, 1))
 
 mret <- filter(mret, mcap1 != 'NA')
-mret <- mret %>%
-  arrange(DATE)
-head(mret)
+mret1 <- mret %>%
+  arrange(month)
+head(mret1)
+
+# A) As noted above, the dataset mret7018 provides monthly stock price data over the 1970–
+# 2018 (see the file mret_var for definitions). Using the price variable (find it), construct
+# monthly stock returns for each stock each month based on the formula:
+
 
 # Calculate return by month
+mret1 <- 
+  mret1 %>%
+  mitate(year, )
 mret1 <- mret %>%
   group_by(month,year) %>%
   summarise(EWRET = mean(RET, na.rm = TRUE),
@@ -37,10 +46,41 @@ mret1 <- mret %>%
 head(mret1)
 
 
-# A) As noted above, the dataset mret7018 provides monthly stock price data over the 1970–
-# 2018 (see the file mret_var for definitions). Using the price variable (find it), construct
-# monthly stock returns for each stock each month based on the formula:
-# 
+
+# B) Does the stock market perform differently during the month of February in a leap year
+# vs. a common year? 
+
+mret2 <- mret1 %>%
+  group_by(month) %>%
+  summarise(EWRETD=100*mean(EWRETD, na.rm = TRUE),
+            VWRETD=100*mean(VWRETD, na.rm = TRUE))
+
+mret2m = as.matrix(mret2)
+barplot(mret2m[,2], names.arg=mret2m[,1],
+        xlab="Month", ylab="Market Return", col="blue",
+        main="Stock Market Seasonality", border="red")
+
+
+# get leap years
+leap_years <- mret %>%
+  filter(format(DATE, "%m/%d") == "02/29") %>%
+  distinct(year)
+leap_years
+
+leap_years = unique(leap_years$year)
+leap_years
+
+
+mret3 <-
+  mret1 %>% filter(year  %in% (leap_years))
+
+mret3m = as.matrix(mret3)
+barplot(mret3m[,2], names.arg=mret3m[,1],
+        xlab="Month", ylab="Market Return", col="blue",
+        main="Stock Market Seasonality", border="red")
+
+
+
 
 # B) Does the stock market perform differently during the month of February in a leap year
 # vs. a common year? 
