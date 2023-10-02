828ca5897eeffd504daa5d4b631549bc7c9e2c6f
strangenicholas <nicholaswstrange@gmail.com>
2023-10-02 11:50:48 -0400

    remove old code

--- Nicholas_Strange_assign3.R
+++ Nicholas_Strange_assign3.R
@@ -31,7 +31,7 @@ mret <-
 # deduplicate
 mret %>% distinct()
 
-mret %>% filter(mret, mcap1 != 'NA')
+# mret %>% filter(mret, mcap1 != 'NA')
 
 # View(mret)
 
@@ -48,21 +48,6 @@ top_mcap_sorted <- sort(top_mcap, decreasing = TRUE)
 print(top_mcap_sorted)
 
 # B) Construct the monthly returns of 10 value-weighted dynamic portfolios consisting of the (10, 20, ...,100) stocks with the largest market capitalization as of the end of the previous month. Calculate the correlations of the returns of these portfolios with the returns of the value-weighted market index (VWRETD) over the 1970 –2018 period. Visualize the 10 correlation coefficients (e.g., via histogram). Interpret the results.
-
-port1 <-
-  mret %>%
-  group_by(DATE) %>%
-  slice_max(order_by = mcap1, n = 10) %>%
-  summarise(ARET = mean(RET, na.rm = TRUE))
-
-# View(port1)
-port2 <-
-  mret %>%
-  group_by(DATE) %>%
-  slice_max(order_by = mcap1, n = 20) %>%
-  summarise(ARET = mean(RET, na.rm = TRUE))
-
-
 
 
 #mkt return
@@ -103,25 +88,6 @@ cor(portfolios$port80$ARET, mret2$VWRETD, use = "pairwise.complete.obs")
 cor(portfolios$port90$ARET, mret2$VWRETD, use = "pairwise.complete.obs")
 cor(portfolios$port100$ARET, mret2$VWRETD, use = "pairwise.complete.obs")
 
-# Visualize
-
-
-
-# Calculate correlations for all portfolios
-correlations <- sapply(portfolios, function(port) {
-  cor(port$ARET, mret2$VWRETD, use = "pairwise.complete.obs")
-})
-
-# Create a histogram of the correlation coefficients
-hist(
-  correlations,
-  main = "Correlation Coefficients with VWRETD",
-  xlab = "Correlation Coefficient",
-  col = "skyblue",
-  border = "black",
-  breaks = 20
-)
-
 
 
 # store correlations for all portfolios
@@ -138,6 +104,8 @@ correlations <-
     cor(portfolios$port90$ARET, mret2$VWRETD, use = "pairwise.complete.obs"),
     cor(portfolios$port100$ARET, mret2$VWRETD, use = "pairwise.complete.obs")
   )
+
+# Visualize
 
 # create df with portfolio numbers and correlations
 correlation_data <-
