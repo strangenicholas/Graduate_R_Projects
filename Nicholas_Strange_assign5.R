ebd593c4b5883b898aa9442357f145ef74c595d5
strangenicholas <nicholaswstrange@gmail.com>
2023-10-29 17:36:16 -0400

    Assign 5 init

---
+++ Nicholas_Strange_assign5.R
@@ -0,0 +1,13 @@
+# Predicting crashes in the stock market is notoriously difficult. Based on the mret7018 data
+# set, construct a variable CRASH equal to 1 if the return of a stock in a given month is less
+# than –8% (–0.08), and equal to 0, otherwise. Build a predictive model of CRASH based on
+# the stock market variables in the data set. To reduce computational time, focus on the last
+# 5 years of data.
+# Make sure that all predictive variables are lagged (1,2,3,.. months), so that this is indeed a
+# predictive model. Select the variables carefully. Some possible predictors include past
+# returns, absolute values of returns (volatility), market caps, turnover (trading volume/shares outstanding), 
+# etc. You could also use interactions of some of the predictors.
+# Explore different predictive variables and models, as discussed in this chapter. What is the
+# best AUC that your model achieves? Summarize your best model in a short write up and
+# email to me both the R-code and the write up. 
+
