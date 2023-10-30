9f0abf1fd82edd90f2f08db0a78b94349b62227c
strangenicholas <nicholaswstrange@gmail.com>
2023-10-30 16:27:55 -0400

    create lagged variables and filter data set

--- Nicholas_Strange_assign5.R
+++ Nicholas_Strange_assign5.R
@@ -11,3 +11,52 @@
 # best AUC that your model achieves? Summarize your best model in a short write up and
 # email to me both the R-code and the write up. 
 
+library(dplyr) 
+library(haven)
+library(ggplot2) 
+library(caret) 
+library(pROC) 
+library(randomForest)
+
+## Read in data
+mret <-
+  read_sas("C:/Users/nicho/OneDrive/UCF MS - FinTech/FIN 6779/mret7018.sas7bdat")
+as.data.frame(mret)
+
+mret <- filter(mret, SHRCD %in% c(10, 11))
+
+# Remove duplicate records
+mret %>% distinct()
+
+mret <- 
+  mret %>% 
+  group_by(PERMNO) %>%
+  mutate(year = as.numeric(format(DATE,"%Y")), 
+         month = as.numeric(format(DATE,"%m")), 
+         PRC = abs(PRC), 
+         sz = PRC*SHROUT, 
+         mcap = lag(sz, 1),
+         pr1m = lag(RET,1), #Past return 1 month
+         pr2m = lag(RET,2), #Past return 2 month
+         pr3m = lag(RET,3), #Past return 3 month
+         pr4m = lag(RET,4), #Past return 4 month
+         pr5m = lag(RET,5), #Past return 5 month
+         v1m = abs(pr1m), #Volatility 1 month prior
+         v2m = abs(pr2m), #Volatility 2 month prior
+         v3m = abs(pr3m), #Volatility 3 month prior
+         v4m = abs(pr4m), #Volatility 4 month prior
+         v5m = abs(pr5m), #Volatility 5 month prior
+         CRASH = if_else(RET <= -.08, 1,0))%>% 
+  ungroup 
+
+mret <- filter(mret, mcap != 'NA') 
+mret <- filter(mret, year >= (max(year)-5))
+
+View(mret)
+
+colnames(mret)
+
+
+## Non-linear regression    
+fit <- lm(CRASH ~ PRC, sz, mcap, pr1m, pr2m, pr3m, pr4m, pr5m, v1m, v2m, v3m, v4m, v5m, data=mret)
+summary(fit) # show results 
