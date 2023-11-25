0f12a45da64a80e4fa42072151636592a5eceb56
strangenicholas <nicholaswstrange@gmail.com>
2023-11-24 23:27:06 -0500

    Load Data

--- Nicholas_Strange_assign7.R
+++ Nicholas_Strange_assign7.R
@@ -1,7 +1,16 @@
 # The file nj2000.sas7bdat contains variables for a sample of household
-# heads from the state of NJ. Definitions of the variables could be found in the filenj2000_var. 
+# heads from the state of NJ. Definitions of the variables could be found in the file nj2000_var. 
   #Based on this information:
 # 1. Filter out observations for the Philadelphia, PA/NJ metropolitan area (METAREA = 616)
 # 2. Using the variable OWNERSHIP, construct a variable equal to 1 for owning a home, vs. 0 for renting
 # 3. Predict homeownership based on the available variables and your knowledge acquired in class. 
   # Pick up your best model based on AUC. Describe the model. 
+
+
+library(dplyr) 
+library(haven)
+
+## Read in data
+cmpst <-
+  read_sas("C:/Users/nicho/OneDrive/UCF MS - FinTech/FIN 6779/nj2000.sas7bdat")
+as.data.frame(cmpst)
