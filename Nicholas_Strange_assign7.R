40b07c9eb5d16525093e15c8d5ad00bf122259ee
strangenicholas <nicholaswstrange@gmail.com>
2023-11-26 11:28:30 -0500

    build predictive models and compare performance

--- Nicholas_Strange_assign7.R
+++ Nicholas_Strange_assign7.R
@@ -9,6 +9,10 @@
 
 library(dplyr) 
 library(haven)
+library(ggplot2) 
+library(caret) 
+library(pROC) 
+library(randomForest)
 
 ## Read in data
 demog <-
@@ -170,4 +174,191 @@ demog2 <-
   demog %>% 
   select(homeown, iscitizen, edulvl, ancestry, employed, hhincome, totincome, industry, occupation, married, travtime, SEX) %>% 
   mutate(homeown = as.factor(homeown))
-summary(homeown) 
+summary(demog2) 
+
+# removing rows with missing values
+demog2 <- na.omit(demog2)
+
+### Random number generation
+set.seed(2)
+rnorm(5)
+rnorm(5)
+
+set.seed(2)
+rnorm(5)
+
+
+### Data splitting 
+set.seed(123)
+
+inTrain <- createDataPartition(demog2$homeown, p = .70, list = FALSE)
+inTrain[1:10]
+
+Train <- demog2[ inTrain,]
+Test  <- demog2[-inTrain,]
+summary(Train)
+summary(Test)
+
+### Train a model 
+glm <- train(homeown ~  iscitizen + edulvl + ancestry + employed + hhincome 
+             + totincome + industry + occupation + married + travtime + SEX,
+             family=binomial(link='logit'), 
+             method = "glm", data = Train)
+glm 
+
+
+# Obtaining details about each model 
+getModelInfo("glm") 
+
+varimpGLM = varImp(glm)
+varimpGLM
+plot(varimpGLM, main = "Mret Variable Importance: GLM") 
+
+### Creating ROC curves 
+# Output the probabilities for 1   
+fitProb <- predict(glm, Test, type = "prob")
+head(fitProb)
+
+fitProb1 <- fitProb$"1"
+par(pty = "s") 
+
+roc(Test$homeown, fitProb1, plot=TRUE, legacy.axes=TRUE, col="#377eb8", 
+    print.auc=TRUE)
+
+# Explore different thresholds with CARET      
+fitProbT80 <- factor(ifelse(fitProb1 >= 0.8, 1, 0))
+
+confusionMatrix(reference = Test$homeown, data = fitProbT80, 
+                mode = "everything",
+                positive = "1") 
+# Explore different thresholds with CARET      
+fitProbT80 <- factor(ifelse(fitProb1 >= 0.8, 1, 0))
+
+confusionMatrix(reference = Test$homeown, data = fitProbT80, 
+                mode = "everything",
+                positive = "1") 
+
+# Explore different thresholds with pROC 
+par(pty = "s") 
+roc.out <- roc(Test$homeown, fitProb1, plot=TRUE)
+summary(roc.out) 
+
+roc.df <- data.frame(
+  Sensitivity=roc.out$sensitivities,
+  Specificity=roc.out$specificities, 
+  fpp=1-roc.out$specificities, 
+  Threshold=roc.out$thresholds) 
+
+roc.dfT <- roc.df %>% 
+  filter(Threshold > 0.79 & Threshold < 0.81)
+roc.dfT 
+
+
+### Choosing an optimal threshold 
+# Extracting Accuracy form the CM 
+Accuracy <- function(t){
+  fitProbT <- factor(ifelse(fitProb1 >= t, 1, 0))
+  cm <- caret::confusionMatrix(reference = Test$homeown, 
+                               data = fitProbT, 
+                               mode = "everything",
+                               positive = "1") 
+  a <- c(cm$overall[['Accuracy']]) 
+  return(a)
+}
+
+T=0
+acc=Accuracy(0.001) 
+
+for(i in seq(1,9)){
+  if(acc < Accuracy(i/10)){
+    T=i/10  
+    acc=Accuracy(i/10)} 
+} 
+
+Tacc <- c(T,acc)
+Tacc
+
+### Once you choose a threshold, you can classify new observations 
+# Simulate new observations 
+
+demog2new <- demog2 %>% 
+  select(homeown, iscitizen, edulvl, ancestry, employed, hhincome, totincome, industry, occupation, married, travtime, SEX) %>% 
+  sample_frac(0.003)
+demog2new 
+
+# Classify the observations @ T=0.4 
+fitProbNew <- predict(glm, demog2new, type = "prob")
+
+fitProbNew <- fitProbNew  %>% 
+  mutate(homeown = if_else(fitProbNew$"1">0.4,1,0))
+fitProbNew 
+
+# CARET finds optimal thresholds  
+fit <- predict(glm, Test)
+fit[1:10]
+
+confusionMatrix(reference = Test$homeown, data = fit, 
+                mode = "everything",
+                positive = "1") 
+
+
+### Comparing models: GLM vs. Random forest
+rf <- train(homeown ~  iscitizen + edulvl + ancestry + employed + hhincome 
+            + totincome + industry + occupation + married + travtime + SEX,
+            method = "rf", data=Train, ntree = 100)
+rf
+
+fitProbRF <- predict(rf, Test, type = "prob")
+head(fitProbRF)
+
+fitProbRF1 <- fitProbRF$"1"
+
+par(pty = "s")
+roc(Test$homeown, fitProbRF1, plot=TRUE, legacy.axes=TRUE, col="#377eb8",
+    print.auc=TRUE)
+
+
+## ROC: GLM & RF
+par(pty = "s")
+roc(Test$homeown, fitProb1, plot=TRUE, legacy.axes=TRUE, col="#377eb8",
+    print.auc=TRUE)
+
+roc(Test$homeown, fitProbRF1, plot=TRUE, legacy.axes=TRUE,  col="#4daf4a",
+    add=TRUE, print.auc=TRUE, print.auc.y=0.4)
+
+legend("bottomright", legend=c("GLM Model",
+                               "RF Model"),
+       col=c("#377eb8", "#4daf4a"), lwd=3)
+
+
+### Comparing models: GLM vs. GLM2
+
+glm <- train(homeown ~  iscitizen + edulvl + ancestry + employed + hhincome 
+             + totincome + industry + occupation + married + travtime + SEX,
+             method = "glm", data=Train)
+glm
+
+varimpGLM = varImp(glm)
+varimpGLM
+plot(varimpGLM, main = "MRET Variable Importance: GLM")
+
+
+# Drop unimportant variables
+glm2 <- train(homeown ~  iscitizen +  ancestry + hhincome + married,
+              method = "glm", data=Train)
+glm2
+
+fitProb2 <- predict(glm2, Test, type = "prob")
+fitProb21 <- fitProb2$"1"
+
+## ROC: GLM & GLM2
+par(pty = "s")
+roc(Test$homeown, fitProb1, plot=TRUE, legacy.axes=TRUE, col="#377eb8",
+    print.auc=TRUE)
+
+roc(Test$homeown, fitProb21, plot=TRUE, legacy.axes=TRUE,  col="#4daf4a",
+    add=TRUE, print.auc=TRUE, print.auc.y=0.4)
+
+legend("bottomright", legend=c("GLM Model",
+                               "NL GLM Model"),
+       col=c("#377eb8", "#4daf4a"), lwd=3)
